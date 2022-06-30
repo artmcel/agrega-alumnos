9a65b2cbcb7e9c1a0392cdcf7c219ffd716d6fa9
WebMaster <umrec_web@unimex.edu.mx>
2022-06-30 13:36:01 -0500

    add fetch for estado and nivel

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -1,3 +1,42 @@
 /**
  * archivo para peticiones fetch...
  */
+
+const url = `http://192.168.1.123/examen/php/functions`;
+
+/**
+ * call carreras
+ */
+
+const carreras = async()=>{
+
+    const resp = await fetch( `${url}?req=ca` );
+
+    const datos = await resp.json();
+    return datos;
+
+
+
+};
+
+const estadoCivil = async()=>{
+
+    const resp = await fetch( `${url}?req=ec`);
+    const datos = await resp.json();
+    return datos;
+};
+
+const nivel = async()=>{
+
+    const resp = await fetch( `${url}?req=ne`);
+    const datos = await resp.json();
+    return datos;
+};
+
+
+export{
+    carreras,
+    estadoCivil,
+    nivel
+}
+
