ad1ae7f57dae6e9e93c43f7a1f3fcb136a930127
WebMaster <umrec_web@unimex.edu.mx>
2022-07-01 17:55:52 -0500

    add bootstrap js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 //import styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@popperjs/core';
 //import bootsrap si lo necesitas
 
 /**
