8134f83af1bbf765f92b7ef4e27aac16686588a0
WebMaster <umrec_web@unimex.edu.mx>
2022-07-04 12:46:39 -0500

    change name to listado.js

--- src/listado-alumnos.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@popperjs/core';
-
-import( /* webpackChunkName: "componentes" */ './js/componentes' );
